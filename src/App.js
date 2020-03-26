27ba59d95cb10e0f4265f13d31e19f2dfe9327bc
ikramsarfraz <m.i.sarfraz@gmail.com>
2020-03-26 12:04:10 -0400

    created initial UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncomeExpense } from "./components/IncomeExpense";
+import { TransactionList } from "./components/TransactionList";
 
 import "./App.css";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpense />
+        <TransactionList />
+      </div>
+    </div>
+  );
 }
 
 export default App;
