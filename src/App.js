df97c976a8924dd8f399c549d1a891616319ee09
ikramsarfraz <m.i.sarfraz@gmail.com>
2020-03-26 12:08:16 -0400

    add transactions component UI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
 import { IncomeExpense } from "./components/IncomeExpense";
 import { TransactionList } from "./components/TransactionList";
+import { AddTransaction } from "./components/AddTransaction";
 
 import "./App.css";
 
@@ -14,6 +15,7 @@ function App() {
         <Balance />
         <IncomeExpense />
         <TransactionList />
+        <AddTransaction />
       </div>
     </div>
   );
