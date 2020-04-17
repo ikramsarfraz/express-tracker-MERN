829554a669d411bf8d3b0241d430991aada64401
ikramsarfraz <m.i.sarfraz@gmail.com>
2020-04-16 22:28:13 -0400

    transactions controller setup

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,3 +1,20 @@
+// @desc    Get all transactions
+// @route   GET /api/v1/transactions
+// @access  PUBLIC
 exports.getTransactions = (req, res, next) => {
   res.send("GET transactions");
+};
+
+// @desc    Add transaction
+// @route   POST /api/v1/transactions
+// @access  PUBLIC
+exports.addTransaction = (req, res, next) => {
+  res.send("POST transactions");
+};
+
+// @desc    Delete transaction
+// @route   DELETE /api/v1/transactions/:id
+// @access  PUBLIC
+exports.deleteTransaction = (req, res, next) => {
+  res.send("Delete transactions");
 };
