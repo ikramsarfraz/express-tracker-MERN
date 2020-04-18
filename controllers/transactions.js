4b3231c3e9cf3f32cd311489b657030029b89e12
ikramsarfraz <m.i.sarfraz@gmail.com>
2020-04-18 14:18:57 -0400

    addTransaction route complete

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -13,7 +13,7 @@ exports.getTransactions = async (req, res, next) => {
       data: transactions,
     });
   } catch (err) {
-    return res.send(500).json({
+    return res.status(500).json({
       success: false,
       error: "Server error",
     });
@@ -24,7 +24,30 @@ exports.getTransactions = async (req, res, next) => {
 // @route   POST /api/v1/transactions
 // @access  PUBLIC
 exports.addTransaction = async (req, res, next) => {
-  res.send("POST transactions");
+  try {
+    const { text, amount } = req.body;
+
+    const transaction = await Transaction.create(req.body);
+
+    return res.status(201).json({
+      success: true,
+      data: transaction,
+    });
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      const messages = Object.values(err.errors).map((val) => val.message);
+
+      return res.status(400).json({
+        success: false,
+        error: messages,
+      });
+    } else {
+      return res.status(500).json({
+        success: false,
+        error: "Server error",
+      });
+    }
+  }
 };
 
 // @desc    Delete transaction
