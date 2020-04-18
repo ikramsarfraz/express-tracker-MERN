587372bc2a5096aea587adf82483e80e10812043
ikramsarfraz <m.i.sarfraz@gmail.com>
2020-04-18 14:08:25 -0400

    getTransactions route working

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,20 +1,35 @@
+const Transaction = require("../models/Transaction");
+
 // @desc    Get all transactions
 // @route   GET /api/v1/transactions
 // @access  PUBLIC
-exports.getTransactions = (req, res, next) => {
-  res.send("GET transactions");
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions,
+    });
+  } catch (err) {
+    return res.send(500).json({
+      success: false,
+      error: "Server error",
+    });
+  }
 };
 
 // @desc    Add transaction
 // @route   POST /api/v1/transactions
 // @access  PUBLIC
-exports.addTransaction = (req, res, next) => {
+exports.addTransaction = async (req, res, next) => {
   res.send("POST transactions");
 };
 
 // @desc    Delete transaction
 // @route   DELETE /api/v1/transactions/:id
 // @access  PUBLIC
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
   res.send("Delete transactions");
 };
