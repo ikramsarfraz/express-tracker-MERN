f28c67d6118384434b98ac5986892b87743e9b90
ikramsarfraz <m.i.sarfraz@gmail.com>
2020-04-20 01:47:49 -0400

    production ready

---
+++ client/build/precache-manifest.b8f15d3fc2e903403be87d40ee4ab2ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea905acaf8d033a33e86ff1d88869ccb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d28f9503f60315552603",
+    "url": "/static/css/main.da56effb.chunk.css"
+  },
+  {
+    "revision": "c81d7c4646bb3b8b69b3",
+    "url": "/static/js/2.77f9f80c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.77f9f80c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d28f9503f60315552603",
+    "url": "/static/js/main.a7f71c46.chunk.js"
+  },
+  {
+    "revision": "51a518e45330f669e104",
+    "url": "/static/js/runtime-main.51eec7e2.js"
+  }
+]);
