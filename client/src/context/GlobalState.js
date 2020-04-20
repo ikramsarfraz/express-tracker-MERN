50cd54e18247c223e91ed0cf3fdafbc32f60e547
ikramsarfraz <m.i.sarfraz@gmail.com>
2020-04-20 00:49:29 -0400

    get transactions action from back

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -1,9 +1,12 @@
 import React, { createContext, useReducer } from "react";
 import AppReducer from "./AppReducer";
+import axios from "axios";
 
 // Initial State
 const initialState = {
-  transactions: []
+  transactions: [],
+  error: null,
+  loading: true,
 };
 
 // Create context
@@ -14,17 +17,33 @@ export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
   // Actions
+  async function getTransactions() {
+    try {
+      const res = await axios.get("/api/v1/transactions");
+
+      dispatch({
+        type: "GET_TRANSACTIONS",
+        payload: res.data.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: "TRANSACTION_ERROR",
+        payload: err.response.data.error,
+      });
+    }
+  }
+
   function deleteTransaction(id) {
     dispatch({
       type: "DELETE_TRANSACTION",
-      payload: id
+      payload: id,
     });
   }
 
   function addTransaction(transaction) {
     dispatch({
       type: "ADD_TRANSACTION",
-      payload: transaction
+      payload: transaction,
     });
   }
 
@@ -33,7 +52,7 @@ export const GlobalProvider = ({ children }) => {
       value={{
         transactions: state.transactions,
         deleteTransaction,
-        addTransaction
+        addTransaction,
       }}
     >
       {children}
